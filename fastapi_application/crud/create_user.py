d83ab65389447dfb6a208fbb4a3ebfaabbdf1e32
karasy0v <antpahalij@gmail.com>
2025-07-08 23:19:33 +0300

    create func to create user

--- fastapi_application/crud/create_user.py
+++ fastapi_application/crud/create_user.py
@@ -7,6 +7,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def create_user(
-    id: int, first_name: str, last_name: str, db: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+    user_id: int,
+    first_name: str,
+    last_name: str,
+    email: str,
+    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> User:
-    pass
+    user_profile = await db.get(User, user_id)
+    if user_profile:
+        return user_profile
+    new_user = User(id=user_id, first_name=first_name, last_name=last_name, email=email)
+    db.add(new_user)
+    await db.commit()
+    return new_user
