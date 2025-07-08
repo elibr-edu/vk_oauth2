8274780d313e0358efca33d68fb7e6e713872b09
karasy0v <antpahalij@gmail.com>
2025-07-08 23:20:36 +0300

    add new func to generate, verify jwt and get current user

--- fastapi_application/crud/generate_secrets.py
+++ fastapi_application/crud/generate_secrets.py
@@ -2,8 +2,15 @@ import secrets
 import hashlib
 import base64
 import aiohttp
-from fastapi import HTTPException, status
+from fastapi import Depends, HTTPException, Request, status
+from sqlalchemy import select
 from fastapi_application.core.config import settings
+from datetime import datetime, timedelta
+from jose import jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi_application.core.models import db_helper
+from fastapi_application.core.models.models import User
+from fastapi_application.core.schemas.token_data import TokenData
 
 
 def generate_code():
@@ -34,15 +41,62 @@ async def exchange_code_for_token(code, device_id, state, code_verifier):
             return await response.json()
 
 
-async def get_vk_data(access_token: str, user_id: int):
+async def get_vk_data(access_token: str):
     async with aiohttp.ClientSession() as session:
-        params = {
+        data = {
             "access_token": access_token,
-            "user_ids": user_id,
-            "v": "5.199",
+            "client_id": settings.vk.client_id,
         }
-        async with session.get("https://api.vk.com/method/users.get", params=params) as response:
+        async with session.post("https://id.vk.com/oauth2/user_info", data=data) as response:
             if response.status != 200:
                 error = await response.text()
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
-            return await response.json()
+            data = await response.json()
+            return data["user"]
+
+
+async def generate_jwt(user_id: int, expire_delta: int = settings.jwt.expire_minutes):
+    payload = {
+        "sub": str(user_id),
+        "exp": datetime.now() + timedelta(expire_delta),
+    }
+    jwt_encode = jwt.encode(payload, settings.jwt.secret_key, settings.jwt.algorithm)
+    return jwt_encode
+
+
+async def verify_jwt(token: str, credentials_exception: Exception):
+    try:
+        jwt_decoded = jwt.decode(token, settings.jwt.secret_key, settings.jwt.algorithm)
+
+        id = jwt_decoded.get("sub")
+        print(id)
+        token_data = TokenData(user_id=str(id))
+
+    except:
+        raise credentials_exception
+    return token_data
+
+
+async def get_current_user(request: Request, db: AsyncSession = Depends(db_helper.session_getter)):
+    cookie = request.cookies.get("auth_token")
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    if not cookie:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Cookie is invalid")
+
+    token = cookie.replace("Bearer", "").strip()
+
+    token_data = await verify_jwt(token, credentials_exception)
+
+    user_query = select(User).where(User.id == int(token_data.user_id))
+
+    query_result = await db.scalars(user_query)
+
+    user = query_result.first()
+
+    return user
