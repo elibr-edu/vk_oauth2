5b20ae4f063f26e6614050f745a270076ef4c054
karasy0v <antpahalij@gmail.com>
2025-07-08 23:21:25 +0300

    edit endpoint vk_login(need update)

--- fastapi_application/crud/oauth.py
+++ fastapi_application/crud/oauth.py
@@ -1,13 +1,18 @@
+from typing import Annotated
+from fastapi_application.core.models import db_helper
 from fastapi_application.crud.generate_secrets import (
     exchange_code_for_token,
     generate_code,
     generate_state,
     get_vk_data,
+    generate_jwt,
 )
+from fastapi_application.crud.create_user import create_user
 from fastapi_application.core.config import settings
 from fastapi.responses import RedirectResponse
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi import status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
@@ -24,7 +29,7 @@ async def redirect_url():
         f"code_challenge_method=S256&"
         f"redirect_uri={settings.vk.redirect_uri}&"
         f"state={state}&"
-        f"scope=email&"
+        f"scope=email"
     )
 
     response = RedirectResponse(auth_url)
@@ -35,11 +40,17 @@ async def redirect_url():
 
 
 @router.get("/vk/auth/callback")
-async def login_vk(code: str, state: str, device_id: str, request: Request):
+async def login_vk(
+    code: str,
+    state: str,
+    device_id: str,
+    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    request: Request,
+):
     cookie = request.cookies.get("vk_state")
 
     if not cookie:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cookie is not found")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Cookie is invalid")
 
     saved_state, code_verifier = cookie.split(":")
 
@@ -48,6 +59,28 @@ async def login_vk(code: str, state: str, device_id: str, request: Request):
 
     token = await exchange_code_for_token(code, device_id, state, code_verifier)
 
-    profile_info = await get_vk_data(access_token=token["access_token"], user_id=token["user_id"])
+    access_token = token["access_token"]
 
-    return profile_info
+    user_info = await get_vk_data(access_token=access_token)
+
+    user_id, first_name, last_name, email = (
+        int(user_info["user_id"]),
+        user_info["first_name"],
+        user_info["last_name"],
+        user_info["email"],
+    )
+
+    user = await create_user(user_id, first_name, last_name, email, db)
+
+    jwt_token = await generate_jwt(user.id)
+
+    # response = RedirectResponse(url="/")
+    # response.set_cookie(
+    #     key="auth_token",
+    #     value=f"Bearer {jwt_token}",
+    #     httponly=True,
+    #     secure=True,
+    #     samesite="lax",
+    #     max_age=7 * 24 * 3600,
+    # )
+    return jwt_token
